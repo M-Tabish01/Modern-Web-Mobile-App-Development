4f685e31c2e5998739461c150476dbfcc7875033
M-Tabish01 <tabish98tk027123@gmail.com>
2026-07-19 18:15:24 +0500

    Ass 1 JS

---
+++ JS/Assignment 1/app.js
@@ -0,0 +1,94 @@
+// Question 1
+
+// var name = prompt("Enter your name ");
+// var agr = Number(prompt("Enter your age"));
+// var city = prompt("Enter your city ");
+
+// console.log("Hello" + name);
+// console.log("you are " + age + "years old");
+// console.log("You live in " + name);
+
+//question 2
+
+// var num1 = Number(prompt("Enter first number"));
+// var num2 = Number(prompt("Enter seconnd number"));
+
+// console.log("The sum is ", num1 + num2);
+
+//question 3
+
+// var num1 = Number(prompt("Enter first number"));
+// var num2 = Number(prompt("Enter seconnd number"));
+
+// console.log("The sum is ", num1 + num2);
+// console.log("The sub is ", num1 - num2);
+// console.log("The multiply is ", num1 * num2);
+// console.log("The divide is ", num1 / num2);
+
+
+//question 4
+
+// var length = Number(prompt("Enter length number"));
+// var width = Number(prompt("Enter width number"));
+// var area = length*width
+
+// console.log("The area of rectangle is " + area);
+
+//qesution 5
+
+// var radius = Number(prompt("Enter radius number"));
+// var circum = 2* Math.PI * radius;
+
+// console.log("The circumference of circle is " + circum);
+
+
+//question 6
+
+// var studentName = prompt("Enter you name");
+// var eng = Number(prompt("Enter english marks"));
+// var math = Number(prompt("Enter math  marks"));
+// var sci = Number(prompt("Enter science  marks"));
+
+// var total = 300;
+// var obtained = eng + math + sci;
+
+// console.log("Name of student: " + studentName);
+// console.log("English marks: " + eng);
+// console.log("math marks: " + math);
+// console.log("science marks: " + sci);
+
+// console.log("Total obtained marks : " + obtained);
+
+
+//question 7
+
+// var age = Number(prompt("Enter your age"));
+// console.log("Your age after 10 years will be ", age + 10);
+
+//question 8
+
+// var amount = Number(prompt("Enter the amount in USD $: "));
+// console.log("The amount in USD ", amount + "$" , " in PKR is: ", amount*285);
+
+//question 9
+
+// var name = prompt("Enter your name: ");
+// var favPL = prompt("Enter your favourite programming language: ");
+
+// console.log("Hello "+ name);
+// console.log("Your favourite programming language is " + favPL + "keep paracticing!");
+
+//question 10
+
+// var pname = prompt("Enter the product name: ");
+// var price  = Number(prompt("Enter the price: "));
+// var quantity = Number(prompt("Enter the quantity: "));
+
+// var total = price * quantity;
+// console.log("Product: "+ pname);
+// console.log("price: "+ price);
+// console.log("Qutantity: "+ quantity);
+// console.log("Total: "+ total);
+
+
+
