cabf9cbd35a45cdcb62ac41b62fde2c90920a618
M-Tabish01 <tabish98tk027123@gmail.com>
2026-07-22 14:55:31 +0500

    ASS 2 completed JS

---
+++ JS/Assignment 2/app.js
@@ -0,0 +1,151 @@
+// Qeustion 1 
+
+// var num = Number(prompt("Enter the number"))
+// if(num > 0){
+    //     console.log("positive");
+    // }else{
+        //     console.log("Negative");
+        // }
+        
+        // question 2
+        
+// var num1 = Number(prompt("Enter the number"))
+// if(num1 % 2 == 0){
+//     console.log("Even");
+// }else{
+//     console.log("Odd");
+// }
+
+// ==============   OR   =======================
+
+// var num1 = Number(prompt("enter the num"))
+// if((num1 & 1)=== 0){
+//     console.log("Even");
+// }else{
+//     console.log("ODd");
+// }
+
+// question 3
+
+// var age = Number(prompt("Enter the age "))
+// if (age > 18){
+//     console.log("Your eligible");
+
+// }else{
+//     console.log("Your not eligible");
+// }
+
+// question 4
+
+// var mark = Number(prompt("Enter the marks"))
+
+// if(mark > 50){
+//     console.log("passed!");
+// }
+// else{
+//     console.log("Fail!");
+// }
+
+// question 5
+
+// var num = Number(prompt("Entre the number"))
+// if(num > 100){
+//     console.log("Large number");
+
+// }else{
+//     console.log("Small number");
+// }
+
+//question 6
+
+// var pass = prompt("Enter the password");
+// if( pass === "admin123"){
+//     console.log("Log in Successful");
+// }else{
+//     console.log("wrong password!");
+// }
+
+// question 7
+
+// var balance = Number(prompt("Enter your balance"))
+// if(balance >= 5000){
+//     console.log("Purchase succesfull");
+// }else{
+//     console.log("Insufficiant balance");
+// }
+
+// question 8
+
+// var temp = Number(prompt("Enter the temperature C: "))
+// if(temp > 30){
+//     console.log("Its hot");
+// }else{
+//     console.log("Weather is pleasent");
+
+// }
+
+
+// question 9
+
+// var num = Number(prompt("Enter the number which is divisible by 5: "))
+// if((num % 5) == 0){
+//     console.log("Divisible by 5");
+// }else{
+//     console.log("not divisible");
+// }
+
+// question 10
+
+// var traffic = prompt("Light of traffic: ");
+// if(traffic === "green"){
+//     console.log("GO");
+// }else{
+//     console.log("STOP");
+// }
+
+// Question 11
+
+// var username = prompt("Enter the username: ");
+// if(username === "admin"){
+//     console.log("Welcome Admin! ");
+// }else{
+//     console.log("Access Denied! ");
+// }
+
+// Question 12
+
+// var height = Number(prompt("Enter the height in CM: "));
+// if(height >= 170){
+//     console.log("Tall");
+// }else{
+//     console.log("Average height");
+    
+// }
+
+//question 13
+
+// var user = true;
+// if(user == false){
+//     console.log("Welcome back");
+// }else{
+//     console.log("Please log in ");
+// }
+
+//question 14
+// var per = Number(prompt("Enter the age: "));
+// if(per >= 60){
+//     console.log("Senior citizen ");
+    
+// }else{
+//     console.log("Not a senior citizen");
+    
+// }
+
+//question 15
+
+// var shop = true;
+// if(shop == true){
+//     console.log("shop is open ");
+// }else{
+//     console.log("shop is closed  ");
+// }
