257f084d3e6b41ec6888193281e722c501622c71
M-Tabish01 <tabish98tk027123@gmail.com>
2026-07-29 15:09:22 +0500

    JS ASSIGNMENT 3

---
+++ JS/Assignment 3/app.js
@@ -0,0 +1,93 @@
+// Question 1 //Write a program that prints numbers from 1 to 10 using a for loop.
+
+// for(var i = 1; i<=10;i++){
+//     console.log(i);
+// }
+
+// Question 2 Write a program that prints numbers from 10 to 1 using a for loop.
+
+// for(var i = 10; i>=1;i--){
+//     console.log(i);
+// }
+
+// Question 3  Write a program that prints "Hello, JavaScript!" 5 times using a for loop.
+
+// for(var i = 1; i<=5;i++){
+//     console.log("Hello, JavaScript!");
+// }
+
+// Question 4   Given the array below, print all its elements using a for loop.
+
+// var fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// for(var i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+
+// }
+
+// Question 5  Given the array below, calculate and print the total marks.
+
+// var marks = [50, 60, 70, 80];
+
+// var total = 0;
+
+// for(var i = 0; i < marks.length; i++){
+//     total = total + marks[i];
+
+// }
+
+// console.log("Total sum",total);
+
+// Question 6  Using the same array below, calculate and print the average marks.
+
+// var marks = [50, 60, 70, 80];
+
+// var total = 0;
+
+// for(var i = 0; i < marks.length; i++){
+//     total = total + marks[i];
+
+// }
+
+// console.log("Total sum",total);
+// console.log("Average of sum",total/marks.length);
+
+
+// Question 7      Given the array below, print all elements in reverse order using a for loop.
+
+// var colors = ["Red", "Green", "Blue", "Yellow"];
+
+// for(var i = colors.length-1; i>=0; i--){
+//     console.log(colors[i])
+// }
+
+// Question 8      Given the array below, print each element along with its index.
+
+// var numbers = [5, 10, 15, 20];
+
+// for (var i = 0; i < numbers.length; i++){
+//     console.log("number",numbers[i],"is on",i,"index");
+
+// }
+
+// Question 9  Given the array below, count how many elements it contains without using the number directly.
+
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+// console.log("The length of this arrray is:", cities.length);
+
+
+// Question 10 Given the array below, print only the even numbers.
+
+// var numbers = [2, 5, 8, 11, 14, 17, 20, 28, 32];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     if ((numbers[i] % 2) == 0) {
+//         console.log("This is a even number in this array:", numbers[i]);
+
+//     } else {
+//         // console.log("this is a odd number in this array:" , numbers[i]);
+
+//     }
+// }
+
